0e4a77ebffe11d3a0b0e3bb86ea96019d6725673
aura80 <96388713+aura80@users.noreply.github.com>
2024-11-10 21:44:06 +0000

    Deploying to gh-pages from @ aura80/goit-js-hw-09@27097d1bcf244134bc11159f61638a9d6d3694ee 🚀

---
+++ 01-color-switcher.eeafaee5.js
@@ -0,0 +1,2 @@
+!function(){let t=document.querySelector("body"),e=document.querySelector("button[data-start]"),l=document.querySelector("button[data-stop]"),a=null;e.addEventListener("click",()=>{null!==a&&clearInterval(a),e.disabled=!0,a=setInterval(()=>{t.style.backgroundColor=`#${Math.floor(0xffffff*Math.random()).toString(16).padStart(6,0)}`},1e3)}),l.addEventListener("click",()=>{clearInterval(a),a=null,e.disabled=!1})}();
+//# sourceMappingURL=01-color-switcher.eeafaee5.js.map
